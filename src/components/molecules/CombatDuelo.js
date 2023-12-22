bbadf1d70e306d25d98c106fada1e66a4f89c3bf
Priscila Sartori <priscilasartori2@gmail.com>
2023-12-21 22:30:37 -0300

    refactor: CombatDuelo

--- src/components/molecules/CombatDuelo.js
+++ src/components/molecules/CombatDuelo.js
@@ -3,26 +3,47 @@ import styles from '../styles/CombatDuelo.module.css';
 import { Lightbulb, FitnessCenter, Speed, Security, FlashOn, SportsKabaddi } from '@mui/icons-material';
 
 function CombatDuelo({ heroes }) {
-  
+  if (!heroes || heroes.length !== 2) {
+    return null;
+  }
+
+  const [hero1, hero2] = heroes;
+
+  const renderPowerStats = (stats, heroIndex) => (
+    stats &&
+    Object.keys(stats).map((stat, index) => (
+      <div key={index} className={styles.powerstats}>
+        {heroIndex === 0 ? (
+          <>
+            <p>{stats[stat]}</p>
+            {stats[stat] > hero2.powerstats[stat] ? (
+              <div className={styles.greenBall}></div>
+            ) : (
+              <div className={styles.redBall}></div>
+            )}
+          </>
+        ) : (
+          <>
+            {stats[stat] > hero1.powerstats[stat] ? (
+              <div className={styles.greenBall}></div>
+            ) : (
+              <div className={styles.redBall}></div>
+            )}
+            <p>{stats[stat]}</p>
+          </>
+        )}
+      </div>
+    ))
+  );
+
   return (
     <div className={styles.container}>
-      <div className={styles.heroiCard}>
-        <h2 className={styles.title}>{heroes[0].name}</h2>
-        <div className={styles.heroiCard2}>
-          <img className={styles.heroiImg} src={heroes[0].images.sm} alt={heroes?.name} />
-          <div className={styles.heroiPowerStats}>
-            {heroes[0].powerstats &&
-              Object.keys(heroes[0].powerstats).map((stat, index) => (
-                <div key={index} className={styles.powerstats}>
-                  <p>{heroes[0].powerstats[stat]}</p>
-                  { 
-                    heroes[0].powerstats[stat] > heroes[1].powerstats[stat] 
-                    ? <div className={styles.greenBall}></div> 
-                    : <div className={styles.redBall}></div>
-                  }
-                </div>
-              ))
-            }
+      <div className={styles.HeroCard}>
+        <h2 className={styles.title}>{hero1.name}</h2>
+        <div className={styles.heroCard2}>
+          <img className={styles.heroImg} src={hero1.images.sm} alt={hero1.name} />
+          <div className={styles.heroPowerStats}>
+            {renderPowerStats(hero1.powerstats, 0)}
           </div>
         </div>
       </div>
@@ -36,24 +57,13 @@ function CombatDuelo({ heroes }) {
         <p><SportsKabaddi style={{ fontSize: 15, marginRight: 8 }}/>Combat</p>
       </div>
       
-      <div className={styles.heroiCard}>
-        <h2 className={styles.title}>{heroes[1].name}</h2>
-        <div className={styles.heroiCard2}>
-        <div className={styles.heroiPowerStats}>
-            {heroes[1].powerstats &&
-              Object.keys(heroes[1].powerstats).map((stat, index) => (
-                <div key={index} className={styles.powerstats}>
-                  { 
-                    heroes[1].powerstats[stat] > heroes[0].powerstats[stat] 
-                    ? <div className={styles.greenBall}></div> 
-                    : <div className={styles.redBall}></div>
-                  }
-                  <p>{heroes[1].powerstats[stat]}</p>
-                </div>
-              ))
-            }
+      <div className={styles.HeroCard}>
+        <h2 className={styles.title}>{hero2.name}</h2>
+        <div className={styles.heroCard2}>
+          <div className={styles.heroPowerStats}>
+            {renderPowerStats(hero2.powerstats, 1)}
           </div>
-          <img className={styles.heroiImg2} src={heroes[1].images.sm} alt={heroes?.name} />
+          <img className={styles.heroImg2} src={hero2.images.sm} alt={hero2.name} />
         </div>
       </div>
     </div>
